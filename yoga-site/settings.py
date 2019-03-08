facb2ccd4e2a4aced2a0de69f5f1de7511fc3b24
Jon Cinque <jon.cinque@gmail.com>
2019-03-08 19:19:01 +0100

    Update conf and settings files

--- yoga-site/settings.py
+++ yoga-site/settings.py
@@ -186,7 +186,7 @@ STATICFILES_FINDERS = (
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = STATIC_URL + "media/"
+MEDIA_URL = "/media/"
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
